60934f05700a04290b9d5090a5219225e9f8540b
KleinKeEper <kranmerramos@upeu.edu.pe>
2020-07-08 12:05:09 -0500

    iniciando

---
+++ src/test/java/pe/edu/upeu/movilidad/MovilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.movilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
